d03d9c61c7c5aa0c429bae44c4a2a864ac2ccde9
Brian Wheat <brianpatrickwheat@gmail.com>
2016-01-14 16:09:45 -0500

    initial commit

---
+++ OtherOrlandoH3/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OtherOrlandoH3.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult RedDress()
+        {
+            return View();
+        }
+
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+
+        public ActionResult Hareline()
+        {
+            return View();
+        }
+
+        public ActionResult Mismanagement()
+        {
+            return View();
+        }
+        
+        public ActionResult Guidelines()
+        {
+            return View();
+        }
+        
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ OtherOrlandoH3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OtherOrlandoH3.MvcApplication" Language="C#" %>
